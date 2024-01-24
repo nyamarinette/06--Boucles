fa8b471cf3a517d9b59e2b5af8134bfe90ef34cf
nyamarinette <nyamarinette2020@gmail.com>
2024-01-24 17:06:25 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //je déclare une variable i a 0, affiche moi toutes les valeurs qui sont différentes de 10.
+        int i = 0;
+        while(i != 10)
+        {
+            i++;
+            if(i==6)
+        System.out.println(i);
+            i++; // incrimentation
+        }
+    }
+}
